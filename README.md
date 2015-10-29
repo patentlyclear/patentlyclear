5dc8bf72b06f75e9e7086d6703d724a001b6f9af
Kiernan McGowan <timmcgowan625@gmail.com>
2015-10-28 20:56:42 -0500

    Initial commit of public release

---
+++ README.md
@@ -0,0 +1,160 @@
+patentlyclear
+===
+
+Patently Clear is an API for the US patent office. Patently Clear provides easy access to the data through full text search and citation graphs. Check out our [examples on tonic](https://tonicdev.com/patentlyclear/) to see the library in action.
+
+[Register for an early access token here.](http://patentlyclear.com/)
+
+`npm install patentlyclear`
+
+Usage
+---
+
+```js
+var PatentlyClear = require('patentlyclear');
+// You can either pass in your API token into the constructor or have it set as an environment variable
+// The library will look for process.env.PC_API_TOKEN if no api token is provided.
+var pc = new PatentlyClear(/* your api token */);
+```
+
+### Looking up by ID
+
+**getById(id, callback)**
+Looks up a document by its id
+Supplying an id that points to multiple resources (such as an application number)
+will return all forms of that document.
+
+Application ID
+* `api.getById('US20140000001') => {application: {...}}`
+
+Grant ID
+* `api.getById('US8877876') => {grant: {...}}`
+
+Application Number (both string and number formats are accepted)
+* `api.getById('13/538,394') => {application: {...}, grant: {...}}`
+* `api.getById(13538394) => {application: {...}, grant: {...}}`
+
+```js
+pc.getById('US8046721', function(err, data) {
+  // data => {grant: {id:..., title:..., abstract:...}}
+});
+```
+
+### Full text search
+
+**search(payload, callback)**
+Search for a set of documents with a query payload.
+
+There are several fields that can be searched on. They are:
+* terms [string]: Perform a full text search with a set of keywords.
+* inventor [string]: Search on inventor name.
+* assignee [string]: Who owns the document.
+* filing_date {before: Date, after: Date}: When the document was filed.
+* publication_date {before: Date, after: Date}: When the document was published.
+* type: [application || grant]: What sort of document it is.
+* size int: Total number of documents to return. (Max = 100)
+
+Fields are 'AND'-ed together for search queries, but the term field can support 'OR'-ing.
+
+```js
+pc.search({terms: 'dna'}, function(err, data) {
+  // data => {hits: [{grant: {}}, {application: {}}]}
+});
+
+// 'dna' AND 'Genentech'
+
+{terms: 'dna', assignee: 'genentech'}
+
+// 'dna' AND (filed during 2013)
+{
+  terms: 'dna',
+  filing_date: {
+    after: '2013-01-01',
+    before: '2014-01-01'
+  }
+}
+
+// 'dna' AND ('cat' OR 'dog' OR 'mouse')
+{
+  terms: [
+    'dna',
+    ['cat', 'dog', 'mouse']
+  ]
+}
+
+```
+
+#### Scrolling
+
+Some result sets can be very large, so pagination is provided by simply re-running the previous search query with the scroll id provided by the returned data.
+
+```js
+var query = {terms: 'dna'};
+pc.search(query, function(err, data) {
+  query.scroll_id = data.scroll_id;
+  pc.search(query, function(err, nextData) {
+    // another set of data
+  });
+});
+```
+
+### Analysis
+
+**analyze(field, payload, callback)**
+Fetches the top trends in a field for a search payload.
+The possible fields are:
+* trends
+* assingee
+* filing_date
+
+```js
+pc.analyze('trends', {terms: 'dna'}, function(err, data) {
+  // data => {hits: [{grant: {}}, {application: {}}]}
+});
+```
+
+### Citation search
+
+**backwardCitation(id, payload, callback)**
+Fetches the backward citations of `id` (eg. the documents that `id` cites.) that match a given search query.
+Optionally provide a search payload to filter documents that are retrieved.
+
+```js
+pc.backwardCitation('US8046721', function(err, data) {
+  // data => {hits: [{grant: {}}, {application: {}}]}
+});
+
+pc.backwardCitation('US8046721', {terms: 'password'}, function(err, data) {
+  // data => {hits: [{grant: {}}, {application: {}}]}
+});
+```
+
+**forwardCitation(id, payload, callback)**
+Fetches the forward citations of `id` (eg. the documents that cite `id`.) that match a given search query.
+
+```js
+pc.forwardCitation('US8046721', function(err, data) {
+  // data => {hits: [{grant: {}}, {application: {}}]}
+});
+
+pc.forwardCitation('US8046721', {terms: 'password'}, function(err, data) {
+  // data => {hits: [{grant: {}}, {application: {}}]}
+});
+```
+
+### Figures
+
+**figure(id, figure_index)**
+Returns (does not callback!) a url that can be placed in an `<img />` to display a figure from a patent.
+Note that `figure_index` starts at 1
+
+```js
+var url = pc.figure('US8046721', 1);
+```
+
+
+License
+---
+
+MIT
+
---
+++ client.js
@@ -0,0 +1,190 @@
+// client.js
+// wrapper for api calls to PatentlyClear
+var request = require('request');
+var _ = require('underscore');
+var url = require('url');
+var pck = require('./package.json');
+
+var apiHost = 'api.patentlyclear.com';
+
+// **api([api_token = process.env.PC_API_TOKEN])**
+// Require-ing this module will return a function that takes your PatentlyClear api token.
+// If no token is passed then the library will attempt to use process.env.PC_API_TOKEN (on the server only).
+// Simply call this function with your token to access the api functions.
+//
+//  `var api = new require('patentlyclear')('my api token')`
+//  `var api = new require('patentlyclear')() // uses process.env.PC_API_TOKEN`
+function contructor(api_token) {
+
+  if (!api_token && process && process.env && process.env.PC_API_TOKEN) {
+    api_token = process.env.PC_API_TOKEN;
+  }
+
+  if (!api_token) {
+    throw new Error('No api_token provided');
+  }
+
+  // **getById(id, callback)**
+  // Looks up a document by its id
+  // Supplying an id that points to multiple resources (such as an application number)
+  // will return all forms of that document.
+
+  // Application ID
+  // * `api.getById('US20140000001') => {application: {...}}`
+
+  // Grant ID
+  // * `api.getById('US8877876') => {grant: {...}}`
+
+  // Application Number (both string and number formats are accepted)
+  // * `api.getById('13/538,394') => {application: {...}, grant: {...}}`
+  // * `api.getById(13538394) => {application: {...}, grant: {...}}`
+  function getById(id, callback) {
+    var formattedUrl = url.format({
+      protocol: 'http',
+      host: apiHost,
+      pathname: id,
+      query: {
+        token: api_token
+      }
+    });
+    request({url: formattedUrl, json: true}, apiResponse(callback));
+  }
+  this.getById = getById;
+
+  // **search(payload, callback)**
+  // Search for a set of documents with a query payload.
+
+  // There are several fields that can be searched on. They are:
+  // * terms [string]: Perform a full text search with a set of keywords.
+  // * inventor [string]: Search on inventor name.
+  // * assignee [string]: Who owns the document.
+  // * filing_date {before: Date, after: Date}: When the document was filed.
+  // * publication_date {before: Date, after: Date}: When the document was published.
+  // * type: [application || grant]: What sort of document it is.
+  // * size int: Total number of documents to return. (Max = 100)
+  function search(payload, callback) {
+    var formattedUrl = url.format({
+      protocol: 'http',
+      host: apiHost,
+      pathname: 'search',
+      query: {
+        token: api_token
+      }
+    });
+    request.post({url: formattedUrl, body: payload, json: true}, apiResponse(callback));
+  }
+  this.search = search;
+
+  // **analyze(field, payload, callback)**
+  // Fetches the top trends in a field for a search payload.
+  function analyze(field, payload, callback) {
+    var formattedUrl = url.format({
+      protocol: 'http',
+      host: apiHost,
+      pathname: 'analyze/' + field,
+      query: {
+        token: api_token
+      }
+    });
+    request.post({url: formattedUrl, body: payload, json: true}, apiResponse(callback));
+  }
+  this.analyze = analyze;
+
+  // **backwardCitation(id, payload, callback)**
+  // Fetches the backward citations of `id` (eg. the documents that `id` cites.) that match a given search query.
+  // Optionally provide a search payload to filter documents that are retrieved.
+  function backwardCitation(id, payload, callback) {
+    if (!callback && typeof payload === 'function') {
+      callback = payload;
+      payload = {};
+    }
+    var formattedUrl = url.format({
+      protocol: 'http',
+      host: apiHost,
+      pathname: 'backward_citation/' + id,
+      query: {
+        token: api_token
+      }
+    });
+    request.post({url: formattedUrl, body: payload, json: true}, apiResponse(callback));
+  }
+  this.backwardCitation = backwardCitation;
+
+  // **forwardCitation(id, payload, callback)**
+  // Fetches the forward citations of `id`. (eg. the documents that cite `id`.)  that match a given search query.
+  function forwardCitation(id, payload, callback) {
+    if (!callback && typeof payload === 'function') {
+      callback = payload;
+      payload = {};
+    }
+    var formattedUrl = url.format({
+      protocol: 'http',
+      host: apiHost,
+      pathname: 'forward_citation/' + id,
+      query: {
+        token: api_token
+      }
+    });
+    request.post({url: formattedUrl, body: payload, json: true}, apiResponse(callback));
+  }
+  this.forwardCitation = forwardCitation;
+
+  // **figure(id, figure_index)**
+  // Returns (does not callback!) a url that can be placed in an `<img />` to display a figure from a patent.
+  // Note that `figure_index` starts at 1
+  function figure(id, figure_index) {
+    return url.format({
+      protocol: 'http',
+      host: apiHost,
+      pathname: 'figure/' + id + '/' + figure_index,
+      query: {
+        token: api_token
+      }
+    });
+  }
+  this.figure = figure;
+
+  // **getVersion()**
+  // Returns the version of this client
+  function getVersion() {
+    return pck.version;
+  }
+  this.getVersion = getVersion;
+
+  return this;
+}
+
+function apiResponse(callback) {
+  return function(err, response, body) {
+    if (err) {
+      return callback(err);
+    }
+
+    if (response.statusCode !== 200) {
+      return callback(body);
+    } else {
+      if (!_.isArray(body)) {
+        body = dateFormat(body);
+      } else {
+        body = body.map(function(o) {
+          return dateFormat(o);
+        });
+      }
+      return callback(null, body);
+    }
+
+  };
+}
+module.exports = contructor;
+
+function dateFormat(obj) {
+  if (obj.application) {
+    obj.application.filing_date = new Date(obj.application.filing_date);
+    obj.application.publication_date = new Date(obj.application.publication_date);
+  }
+  if (obj.grant) {
+    obj.grant.filing_date = new Date(obj.grant.filing_date);
+    obj.grant.publication_date = new Date(obj.grant.publication_date);
+  }
+  return obj;
+}
